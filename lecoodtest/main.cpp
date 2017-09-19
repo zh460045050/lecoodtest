0c6a041c8c5db9ea01f77ae7ea91a77bc78410b6
朱磊 <zhulei@zhuleidemacbook-pro.local>
2017-09-19 17:01:04 +0800

    Initial Commit

---
+++ lecoodtest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lecoodtest
+//
+//  Created by 朱磊 on 2017/9/19.
+//  Copyright © 2017年 朱磊. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
